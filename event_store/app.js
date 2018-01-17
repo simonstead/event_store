b5ac400c6b990dc2e3b18d004158a809fce023ca
Simon Stead <simonstead@me.com>
2018-01-17 10:39:20 +0000

    remove unnecessary files

--- event_store/app.js
+++ event_store/app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const redisClient = require('./redisClient');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
